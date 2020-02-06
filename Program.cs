a15209c02452dd3a684e387ba184b7ad64c1ab13
Jacob Levin <yacob.levin3@gmail.com>
2020-02-06 17:49:05 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Method_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name"); //asks user for name 
+            string input = Console.ReadLine();
+            bool ans = Regex.IsMatch(input, @"^[a-zA-z]+$");
+            if (ans)
+            {
+                Hello(input);
+            }
+            else
+            {
+                Console.WriteLine("Please input a name without numbers");
+
+            }
+            
+        }
+
+        static void Hello(string input)
+        {
+
+            Console.WriteLine("Hello " + input + "!");
+
+            
+
+        }
+    }
+}
